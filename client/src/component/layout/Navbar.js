c4e18971474309c1f340b21d544447e6fd912ad2
Mayank Rajput (HackElite) <78653024+hackelite01@users.noreply.github.com>
2022-09-16 21:57:15 +0530

    MensesMate

---
+++ client/src/component/layout/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Menu, Popup } from "semantic-ui-react";
+import axios from "axios";
+export default function NavBar(props) {
+  const logOut = () => {
+    axios.get("http://localhost:5000/api/auth/logout").then((res) => {
+      // props.setUser(null);
+      props.state.user = null;
+      console.log('logged out');
+      // console.log(props.state);
+      props.handleChangeState(props.state);
+    });
+  };
+  const handleClick = () => {
+    props.state.currentPage = "login";
+
+    props.handleChangeState(props.state);
+  };
+  const ToHome = () => {
+    props.state.currentPage = "Home";
+    props.handleChangeState(props.state);
+  };
+  return (
+    <Menu color="pink" inverted>
+      <Menu.Item onClick={ToHome}>Menses Mate</Menu.Item>
+
+      {props.user === null ? (
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Button onClick={handleClick}>Log In</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      ) : (
+        <Menu.Menu position="right">
+          <Menu.Item>
+            {props.user.email}
+            <Button onClick={() => logOut()} icon="log out" />
+          </Menu.Item>
+        </Menu.Menu>
+      )}
+    </Menu>
+  );
+}
